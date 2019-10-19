0c82e74a4b19329691964e203ad606706708c319
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2019-10-19 07:28:50 -0400

    add TC Calc
    - anticipating this will be an unpopular download

---
+++ Casks/tccalc.rb
@@ -0,0 +1,10 @@
+cask 'tccalc' do
+  version '4'
+  sha256 '48a7482ee0ef6716aa1e3144eb1cc9e2a92a2b5e5ac349bd62efe22647d5d4cc'
+
+  url "https://www.drastic.tv/downloads/tccalc/TCCalc#{version}.dmg"
+  name 'TCCalc'
+  homepage 'https://www.drastic.tv/productsmenu-56/videoiosoftwarelist/tccalc'
+
+  app 'TCCalc.app'
+end
