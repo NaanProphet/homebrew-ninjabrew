9d1fbaaf3ef6a89eb751b03165ecd29defc04da6
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2020-04-30 22:09:13 -0400

    Create loopback1.rb

---
+++ Casks/loopback1.rb
@@ -0,0 +1,12 @@
+cask 'loopback' do
+  version '1.2.1'
+  sha256 '8be69661a740a4966ad7048b5ab68a1322ae8a373b4a15f23d1fc02ed44e32b5'
+
+  url "https://rogueamoeba.com/legacy/downloads/Loopback-#{version.no_dots}.zip"
+  name 'Loopback'
+  homepage 'https://rogueamoeba.com/loopback/'
+
+  depends_on macos: '>= :el_capitan'
+
+  app 'Loopback.app'
+end
