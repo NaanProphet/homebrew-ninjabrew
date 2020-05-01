8aa31fdf657672cbfdb017278ea23a58342e1fef
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2020-04-30 22:01:18 -0400

    Update cineplay 1.6.4.0

--- Casks/cineplay.rb
+++ Casks/cineplay.rb
@@ -1,6 +1,6 @@
 cask 'cineplay' do
-  version '1.6.0.0'
-  sha256 'd460eaa7a46b95775a78da465f882858c05ed4296863ea12505c784830f67ae0'
+  version '1.6.4.0'
+  sha256 '1155a9a84004db53465068bc6b4bd640cec0cab929b8eb1aed9cbe1504cca5c0'
 
   url "https://www.digitalrebellion.com/download/cineplay?version=#{version.no_dots}"
   appcast 'https://www.digitalrebellion.com/cineplay/download',
